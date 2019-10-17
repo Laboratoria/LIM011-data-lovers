656d07e5531b1f1aa1e975b1dbe8b77193da84d9
Betsy Vidal <ing.betsyvies@gmail.com>
2019-10-17 18:17:12 -0500

    add files

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+ 
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',},
+  plugins: ['jest'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 0,
+    'prefer-destructuring': 0,
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+};
