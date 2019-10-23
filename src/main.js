c87410c57b7b79dcec13f49eb845f00c12257a41
Alelizsanchez <Alelizsanchez@gmail.com>
2019-10-23 08:57:56 -0500

    actualiza main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,35 @@
 /**
  * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
+ * import POTTER from './data/potter/potter.js' */
+ import LoL from './data/lol/lol.js'
+ 
 
 import { example } from './data.js';
+
+// import lol from './data/lol/lol.js';
 
 console.log(example);
 
 /*
  * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+ * console.log(POTTER);*/
+
+ console.log(LoL)
+
+ const lolData = LoL.data;
+//  const $root = document.querySelector('#root');
+ 
+ const playing = (data) => {
+    let almacenar = '';
+    for (let i = 0; i < data.length; i++) {
+           let item = `
+           <div class="personajes__flex" name="jalar" id="${data[i].id}">
+           <img class="img" src='${data[i].splash}'/>
+           <p class="decorado">${data[i].name}</p>
+         </div>`;
+      almacenar += item;
+    }
+    return almacenar;
+  };
+
+  document.querySelector('#root').innerHTML = playing(lolData);
