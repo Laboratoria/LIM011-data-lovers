ee8dd5de45aaac7f320cf997f03770c838df2dfa
Naty4393 <naty4393@gmail.com>
2019-10-23 08:56:03 -0500

    update

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
-
-import { example } from './data.js';
+import LoL from './data/lol/lol.js'
+ 
+import { example } from './data.js'
 
 console.log(example);
 
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+console.log(LoL);
+ 
+
