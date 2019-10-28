340954323664e9a23dfc3e0118c94694802b2f78
Gabby97 <gabbacvdor@gmail.com>
2019-10-28 12:30:28 -0500

    Mostrar personajes en la pantalla

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
 
-import { example } from './data.js';
+  // import POKEMON from './data/pokemon/pokemon.js'
+  // import LoL from './data/lol/lol.js'
+  import POTTER from './data/potter/potter.js'
+  console.log(POTTER);
+ 
 
-console.log(example);
+import { pintarPersonajes } from './data.js';
+
+document.getElementById('arrPotter').innerHTML = pintarPersonajes(POTTER);
 
 /*
  * console.log(POKEMON);
