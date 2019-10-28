a3721e39097942cc1d4264705c98dd9a0e57ab63
NataliTC <natali.tauma@gmail.com>
2019-10-28 17:07:41 -0500

    agrega pintame en main

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@
 /* esta es una función de ejemplo */
 /* eslint-disable import/prefer-default-export */
 /* eslint arrow-body-style: ["error", "always"] */
-export const mostrarPokemon = (img, num, name, type, height, weight, candy_count, spaw_time, weaknesses, next_evolution) => {
+export const mostrarPokemon = () => {
   
   return 'mostrarPokemon';
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import POKEMON from './data/pokemon/pokemon.js';
 import { mostrarPokemon } from './data.js';
 
-console.log(POKEMON);
+const pintandoPokemon = document.getElementById('pintame');
+  let misPokemones = '';
+    POKEMON.forEach(pintar =>{
+        misPokemones += `
+        <div class = "contenedor">
+        <img src ="${pintar.img}"/>
+        <p>${pintar.id}</p>
+        <p>${pintar.name}</p>
+        </div>
+        `;
+  pintandoPokemon.innerHTML = misPokemones;
+})
 
-document.querySelector('#insertar-pokemones').innerHTML = mostrarPokemon(POKEMON);
+console.log(POKEMON);
