e61a02d935a2ec7708928eff5b890d30f3c718ae
krocarondinel <rocarondinel.kelly@gmail.com>
2019-10-28 12:37:20 -0500

    pinta todos los personajes

--- src/data.js
+++ src/data.js
@@ -5,3 +5,18 @@
 export const example = () => {
 return 'example';
 };
+
+export const mostrarTodosPersonajes = (dataPotter) => {
+  let templatePotter = '';
+  dataPotter.map(extrae =>{
+    templatePotter += `
+          <div>
+            <div id= "${extrae.name}" name = "potter">
+              <img class="imagen" src= "${extrae.image}"/>
+            </div>
+            <p>${extrae.name}</p>
+            <p>${extrae.house}</p>
+          </div>`;
+  });
+  return templatePotter;
+};
--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
-import POTTER from './data/potter/potter.js'
-import { example } from './data.js';
-
-const baseDatosPotter = POTTER 
-
-const personaje = baseDatosPotter[1]
-const ver = personaje.name 
-console.log(example);
-console.log(POTTER);
-console.log(baseDatosPotter.slice(0,1));
-const fullNames = baseDatosPotter.map(personaje => `${personaje.name}, ${personaje.gender}`)
-
-console.log(fullNames);
+import POTTER from './data/potter/potter.js';
+import {mostrarTodosPersonajes} from './data.js';
+document.querySelector('#mostrado').innerHTML = mostrarTodosPersonajes(POTTER);
 
