311527821ec594acd1e5f448c9a1a6fa4e9af074
Naty4393 <naty4393@gmail.com>
2019-10-29 08:09:48 -0500

    update1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import LoL from './data/lol/lol.js'
  
 import { example } from './data.js'
 
+const name = document.getElementById('name')
+
+name.innerHTML = Object.values(LoL.data.Aatrox)
+
+console.assert(LoL)
 console.log(example);
 
 console.log(LoL);
