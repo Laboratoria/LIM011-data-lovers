9e91e93c45eff99062df32b46b19d32362ca1732
mishelpa <judith.paredesartica@gmail.com>
2019-10-29 08:24:59 -0500

    Ingresar cambios en archivos js y html

--- src/data.js
+++ src/data.js
@@ -1,7 +1,29 @@
+// import pokemon from "./data/pokemon/pokemon"
+
 /* Manejo de data */
 
 // esta es una función de ejemplo
 
-export const example = () => {
-  return 'example';
+/* export const datosPokemon = (datos) => {
+  datos.map((listado) => {
+    const newDiv = document.createElement('div');
+    const newDiv2 = document.createElement('div');
+    const imagen = document.createElement('img');
+    imagen.setAttribute('src', listado.img);
+    const newContent = document.createTextNode(listado.num + ' ' + listado.name);
+    newDiv2.appendChild(newContent); // añade texto al div creado.
+    newDiv.appendChild(newDiv2);
+    newDiv.appendChild(imagen);// añade la imagen al div creado
+    document.body.appendChild(newDiv);// añade los div y su contenido a la pagina
+  });
+  return datosPokemon;
+}; */
+
+export const mostrarListaPokemon = (dataPoke) => {
+  const pokedex = dataPoke.map((data) => ({ num: data.num, img: data.img, name: data.name }));
+  // eslint-disable-next-line no-console
+  console.log(pokedex);
 };
+
+// eslint-disable-next-line no-console
+console.log(pokedex);
--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
 
-import { example } from './data.js';
+import POKEMON from './data/pokemon/pokemon.js';
 
-console.log(example);
+// import { datosPokemon } from './data.js';
+// import pokemon from './data/pokemon/pokemon.js'
 
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+let i = 0;
+for (i = 0; i < POKEMON.length; i += 1) {
+  const newDiv = document.createElement('div');
+  const newDiv2 = document.createElement('div');
+  const imagen = document.createElement('img');
+  newDiv.setAttribute('id', 'contenedor-pokemon');
+  newDiv2.setAttribute('id', 'datos-pokemon');
+  imagen.setAttribute('src', POKEMON[i].img);
+  const newContent = document.createTextNode(POKEMON[i].num + ' ' + POKEMON[i].name);
+  newDiv2.appendChild(newContent); // añade texto al div creado.
+  newDiv.appendChild(newDiv2);
+  newDiv.appendChild(imagen);
+  document.body.appendChild(newDiv);
+}
+
+// añade la imagen al div creado
+// document.body.appendChild(newDiv); // añade los div y su contenido a la pagina
