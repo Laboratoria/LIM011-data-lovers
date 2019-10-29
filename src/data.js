e15f5e592a3b7dff0ea5984bcf77c2c90e65193c
MelissaCcoyllo <mel.ccoyllo@gmail.com>
2019-10-28 23:43:03 -0500

    ordena pokemones

--- src/data.js
+++ src/data.js
@@ -3,6 +3,5 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint arrow-body-style: ["error", "always"] */
 export const mostrarPokemon = () => {
-  
   return 'mostrarPokemon';
 };
--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 import POKEMON from './data/pokemon/pokemon.js';
-import { mostrarPokemon } from './data.js';
 
 const pintandoPokemon = document.getElementById('pintame');
-  let misPokemones = '';
-    POKEMON.forEach(pintar =>{
-        misPokemones += `
+let misPokemones = '';
+POKEMON.forEach(pintar => {
+  misPokemones += `
+        
         <div class = "contenedor">
         <img src ="${pintar.img}"/>
-        <p>${pintar.id}</p>
-        <p>${pintar.name}</p>
+        <p><b>${pintar.id} ${pintar.name}</b></p>
+        <p>Altura: ${pintar.height}</p>
+        <p>Peso: ${pintar.weight}</p>
+        <p>Caramelos: ${pintar.candy_count}</p>
+        <p>Tiempo de Aparición: <br>${pintar.spawn_time}</p>
+        <p>Debilidades:<br>${pintar.weaknesses}</p>
         </div>
         `;
   pintandoPokemon.innerHTML = misPokemones;
-})
+});
 
 console.log(POKEMON);
