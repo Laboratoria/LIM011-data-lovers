761ea42bba7f278a4abc06182f7cfff423287e96
Vilmarys Rodriguez <vilmango@hotmail.com>
2019-10-29 10:38:55 -0500

    Actualiza index.html, data.js y main.js
    Se importó y exportó datos de pokemon.js y leerlos y mostrarlos en el html

--- src/data.js
+++ src/data.js
@@ -2,6 +2,24 @@
 
 // esta es una función de ejemplo
 
-export const example = () => {
-  return 'example';
+export const showPokemon = (data) => {
+  let pokemon ='' 
+  ;
+  data.map(objeto => {
+    pokemon += `
+    <div id="${objeto.id}" name="pokemon"><p>
+    <img src="${objeto.img}"/></p>
+    <p>Nombre: ${objeto.name}</p>
+    <p>Peso: ${objeto.weight}</p>
+    <p>Tipo: ${objeto.type.join('  ')}</p>
+    </div>
+    `
+  });
+  //console.log(plantillaPokemon);
+  return pokemon;
 };
+
+/*const valorCaramelo = (data) =>{
+  let cuantosCaramelos= `${objeto.candy_count}`;
+  let pokemonBuscado =   console.log(cuanosCaramelos);
+};*/
