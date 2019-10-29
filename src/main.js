b30e1a685cfab09aa829b85c488d14ebf060f54e
Alelizsanchez <Alelizsanchez@gmail.com>
2019-10-29 08:40:22 -0500

    actualiza index.html main.js style.css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 /**
  * import POKEMON from './data/pokemon/pokemon.js'
  * import POTTER from './data/potter/potter.js' */
- import LoL from './data/lol/lol.js'
- 
+import LoL from './data/lol/lol.js'
+
 
 import { example } from './data.js';
 
@@ -14,22 +14,21 @@ console.log(example);
  * console.log(POKEMON);
  * console.log(POTTER);*/
 
- console.log(LoL)
+console.log(LoL)
+let lolData = Object.values(LoL.data);
+//  const lolData = LoL.data;
+ const $root = document.querySelector('#root');
 
- const lolData = LoL.data;
-//  const $root = document.querySelector('#root');
- 
- const playing = (data) => {
+const playing = (data) => {
     let almacenar = '';
     for (let i = 0; i < data.length; i++) {
-           let item = `
-           <div class="personajes" name="id" id="${data[i].id}">
-           <img class="img" src='${data[i].splash}'/>
-           <p class="name">${data[i].name}</p>
-         </div>`;
-      almacenar += item;
+    let item = `<div class="contenedor flex" >
+      <img class="img__lol" src="${data[i].splash}"/>
+      <p class ="name"> ${data[i].name}</p>
+      </div>`;
+        almacenar += item;
     }
     return almacenar;
-  };
+};
 
-  document.querySelector('#root').innerHTML = playing(lolData);
+$root.innerHTML = playing(lolData);
