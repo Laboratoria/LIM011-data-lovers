d3d2979d1e514e44c5f988f60e811d1d0eae1502
Gabby97 <gabbacvdor@gmail.com>
2019-10-29 21:38:56 -0500

    Edicion UI, implementacion template flid card

--- src/data.js
+++ src/data.js
@@ -1,33 +1,10 @@
-export const pintarPersonajes = (datos) => {
-  let pintaImagen = '';
-  datos.forEach(pintar => {
-    pintaImagen += `
-    <div class = "container">
-    <img class="mi-imagen" src = "${pintar.image}"/>
-    <p> ${pintar.name} </p>
-    </div>
-    `;
-  });
-  return pintaImagen;
-};
-
-
-//   arrPotter.forEach((pintar) => {
-//     imagenes.innerHTML += `
-//     <div class = "pintando">
-//     <img src = "${pintar.image}">
-//     <p>${pintar.name} ${pintar.wand.wood.core} ${pintar.patronus}</p>
-//     </div>
-//     `;
-//     return imagenes;
-//   });
-// };
-// pintarPersonajes(arrPotter);
-
 /* Manejo de data */
 
 // esta es una función de ejemplo
 
-// export const example = () => {
-//   return 'example';
-// };
+export const mostrarPersonajesPotter = () => {
+  return '';
+};
+export const mostrarCaracteristicasPotter = () => {
+  return '';
+};
