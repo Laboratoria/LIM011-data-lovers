32df6d57facc60a4e5a99daec397da460f5d2a92
NataliTC <natali.tauma@gmail.com>
2019-11-04 13:09:41 -0500

    agrega MENU por tipos y debilidades

--- src/main.js
+++ src/main.js
@@ -1,73 +1,15 @@
 import POKEMON from './data/pokemon/pokemon.js';
 import { pintado, porTipo, porDebilidades } from './data.js';
 
-const todos = document.querySelector('#todos');
-todos.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = pintado(POKEMON, todos.value);
+document.querySelector('#pintame').innerHTML = pintado(POKEMON);
+
+const selecte1 = document.querySelector('#select-type');
+selecte1.addEventListener('change', () => {
+  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, selecte1.value);
 });
-const Grass = document.querySelector('#Grass');
-Grass.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Grass.value);
-});
-const Water = document.querySelector('#Water');
-Water.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Water.value);
-});
-const Bug = document.querySelector('#Bug');
-Bug.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Bug.value);
-});
-const Electric = document.querySelector('#Electric');
-Electric.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Electric.value);
-});
-const Ghost = document.querySelector('#Ghost');
-Ghost.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Ghost.value);
-});
-const Fire = document.querySelector('#Fire');
-Fire.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Fire.value);
-});
-const Poison = document.querySelector('#Poison');
-Poison.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Poison.value);
-});
-const Flying = document.querySelector('#Flying');
-Flying.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Flying.value);
-});
-const Normal = document.querySelector('#Normal');
-Normal.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Normal.value);
-});
-const Ground = document.querySelector('#Ground');
-Ground.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Ground.value);
-});
-const Fighting = document.querySelector('#Fighting');
-Fighting.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Fighting.value);
-});
-const Psychic = document.querySelector('#Psychic');
-Psychic.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Psychic.value);
-});
-const Rock = document.querySelector('#Rock');
-Rock.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Rock.value);
-});
-const Ice = document.querySelector('#Ice');
-Ice.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porTipo(POKEMON, Ice.value);
-});
-const Iced = document.querySelector('#Iced');
-Iced.addEventListener('click', () => {
-  document.querySelector('#resultado').innerHTML = porDebilidades(POKEMON, Iced.value);
-});
-const Waterd = document.querySelector('#Waterd');
-Waterd.addEventListener('change', () => {
-  document.querySelector('#resultado').innerHTML = porDebilidades(POKEMON, Waterd.value);
+const selecte = document.querySelector('#select-weaknesses');
+selecte.addEventListener('change', () => {
+  document.querySelector('#resultado').innerHTML = porDebilidades(POKEMON, selecte.value);
 });
 const bienvenida = document.getElementById('bienvenida');
 const boton1 = document.getElementById('boton1');
