34222988b823c20e9146dbf26c5b497702e1d5df
Fiorela <fbasiliomanrique@gmail.com>
2019-11-05 12:21:29 -0500

    Terminar modal main.js falta traer caracteristicas

--- src/data.js
+++ src/data.js
@@ -1,40 +1,18 @@
+/* eslint-disable implicit-arrow-linebreak */
 /* Manejo de data */
 
 // esta es una función de ejemplo
-
-// export const showCharacters = () => {
-//   const numero = ['paola', 'alejandra', 'vanesa', 'carla'];
-//   numero.sort();
-//   return numero;
-// };
-export const showCharacters = (characters) => {
-  let templatePotter = '';
-// eslint-disable-next-line no-plusplus
-  for (let i = 0; i < characters.length; i++) {
-    templatePotter += `
-    <div class=content id="content">
-    <div class=styleCharacter>
-    <img class=imageCharacter src=${characters[i].image}>
-    <div class=name>${characters[i].name}</div>
-    </div>
-    </div>
-    `;
-  }
-  return templatePotter;
+export const ascendente = (datos) => {
+// eslint-disable-next-line max-len
+// const ordered = datos.sort((first, second) => (first.name > second.name ? 1 : -1)); funcion ternaria
+// return ordered;
+  const ordered = datos.sort((a, b) => {
+    if (a.name > b.name) {
+      return 1;
+    // eslint-disable-next-line no-else-return
+    } else {
+      return -1;
+    }
+  });
+  return ordered;
 };
-
-export const ascendente = (charactersToOrderAsc) => {
-  console.log(charactersToOrderAsc);
-};
-
-// const ordenAZ = (data) => {
-//   return data.sort((a, b) => {
-//     if (a.name > b.name) {
-//       return 1
-//     } else {
-//     return -1
-//     }
-//   });
-// }
-
-// console.log(ordenAZ);
--- src/main.js
+++ src/main.js
@@ -3,46 +3,89 @@
  * import LoL from './data/lol/lol.js'
  * import POTTER from './data/potter/potter.js'
  */
+
 import POTTER from './data/potter/potter.js';
-// Ejemplo funciona
-// eslint-disable-next-line import/named
-import { showCharacters, ascendente } from './data.js';
-
-document.querySelector('#contentCharacter').innerHTML = showCharacters(POTTER);
-
-document.querySelector('#contentCharacter').innerHTML = ascendente(POTTER);
-
-// Ejemplo funciona
-// const result = showCharacters();
-// console.log(`result: ${result}`);
+import { ascendente } from './data.js';
 // console.log(POTTER);
 
-// console.log(showCharacters);
+const contentCharacter = document.getElementById('contentCharacter');
+const filterMenu = document.getElementById('filterMenu');
+const imageIcon = document.getElementById('imageIcon');
+const selectAlfab = document.getElementById('selectAlfab');
+const modal = document.getElementById('modal');
+let flex = document.getElementById('flex');
+// const openModal = document.getElementById('openModal');
+let close = document.getElementById('close');
+
+const showCharacters = (dataPotter) => {
+  let templatePotter = '';
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < dataPotter.length; i++) {
+    templatePotter += `
+    <div class=content id="content">
+    <div class=styleCharacter id="openModal">
+    <img class=imageCharacter src=${dataPotter[i].image}>
+    <div class=name>${dataPotter[i].name}</div>
+    </div>
+    </div>
+    `;
+  }
+  return templatePotter;
+};
+contentCharacter.innerHTML = showCharacters(POTTER);
+
+// Modal
+const abrirModal = () => {
+  // const newArray = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < POTTER.length; i++) {
+    // eslint-disable-next-line no-const-assign
+    // eslint-disable-next-line no-unused-expressions
+    POTTER[i];
+  }
+  // console.log(newArray);
+};
+
+// openModal.addEventListener('click', () => {
+//   modal.classList.add('hide');
+// });
+// Fin modal
+
+contentCharacter.addEventListener('click', () => {
+  abrirModal();
+});
 
 // Barra Lateral
-
-const btnToggle = document.querySelector('.toggle-btn');
-btnToggle.addEventListener('click', () => {
-  document.getElementById('sideBar').classList.toggle('active');
+let open = 0;
+imageIcon.addEventListener('click', () => {
+  if (open === 0) {
+    filterMenu.classList.remove('hide');
+    filterMenu.classList.add('show');
+    open = 1;
+  } else {
+    filterMenu.classList.remove('show');
+    open = 0;
+  }
 });
-// fin BarraLateral
+// Fin de Barra Lateral
 
-// ordenar AZ
+// OrdenarAZ
+selectAlfab.addEventListener('change', (event) => {
+  if (event.target.value === '0') {
+    contentCharacter.innerHTML = showCharacters(ascendente(POTTER));
+  } else {
+    contentCharacter.innerHTML = showCharacters(ascendente(POTTER).reverse());
+  }
+});
+// Fin OrdenarAZ
 
-// const hola = document.getElementById('hola');
-// hola.addEventListener('click', (ascendente)=>{
 
-// });
+// console.log(example);
+// console.log(POTTER);
 
-// const ascendant = document.querySelector('#ascendant');
-// ascendant.addEventListener('click', () => {
-//   const numero = showCharacters();
-//   console.log(`result: ${numero}`);
-// });
 
 /*
+ * console.log(POKEMON);
+ * console.log(LoL);
  * console.log(POTTER)
 */
-// if (event.target.value === '0') {
-//     // contentCharacter.innerHTML = mostrarPoke(ordenarPoke(pokeData));
-//   }
