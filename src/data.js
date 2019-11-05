0f57ccb1cf07695fdbea65c2f48049cadf8a6131
Gabby97 <gabbacvdor@gmail.com>
2019-11-05 12:28:14 -0500

    Filtros terminados

--- src/data.js
+++ src/data.js
@@ -9,23 +9,35 @@
 export const mostrarCaracteristicasPotter = (POTTER) => {
   return POTTER;
 };
+//FILTRO PARA ROLES
+export const filtroPorRoles = (rol, POTTER) => POTTER.filter(e => e[rol]);
 
-// export const filtrar = (POTTER, orderBy) => {
-//   const filtrarOcupacion = POTTER.filter((e) => {
-//     console.log(orderBy);
-//     return e.orderBy === true;
-//   });
-//   // console.log(e);
-//   return filtrarOcupacion;
-//   // console.log(filtrarOcupacion);
-//   // return filtrarOcupacion;
-// };
+//FILTRO PARA CASAS
 
+export const filtroPorCasas = (casa, POTTER) => {
+  const filtraCasa = POTTER.filter(e => e.house === casa)
+  console.log(filtraCasa);
+  return filtraCasa;
+}
+
+export const filtroPorGenero = (genero, POTTER) => {
+  const filtraGenero = POTTER.filter(e => e.gender === genero)
+  return filtraGenero;
+}
+
+// esta funcion ejecuta para  el boton de desplazamiento de casa 
 
 //  PRUEBAAAAA //
-export const probando = (POTTER) => {
-  const pruebita = POTTER.filter((e) => e.hogwartsStudent === true);
-  console.log(pruebita)
-  return pruebita;f
-};
+// export const probando = (POTTER) => {
+//   const pruebita = POTTER.filter((e) => e.hogwartsStudent === true);
+//   console.log(pruebita)
+//   return pruebita;
+// };
+
+// export const probando2 = (POTTER) => {
+//   const pruebita2 = POTTER.filter((e) => e.hogwartsStaff === true);
+//   console.log(pruebita2);
+//   return pruebita2;
+// }
+
 
--- src/main.js
+++ src/main.js
@@ -3,39 +3,63 @@
 import POTTER from "./data/potter/potter.js";
 // console.log(POTTER);
 
-import { mostrarCaracteristicasPotter, probando } from "./data.js";
+import { mostrarCaracteristicasPotter, filtroPorRoles, filtroPorCasas, filtroPorGenero } from "./data.js";
 
 
 const miArrImagenes = document.getElementById("arrPotter");
-let pintaImagen = "";
-POTTER.forEach(pintar => {
-  pintaImagen += `
-    <div class="flip-card">
-      <div class="flip-card-inner">
-        <div class="container">
-          <img class="mi-imagen" src="${pintar.image}"/>
-          <button id="btn-caracteristicas" class="name">${pintar.name}</button>
-        </div>
-        <div class="anverso">
-          <div class="box">
-            <p><strong>Especie:</strong> ${pintar.species}</p>
-            <p><strong>Género:</strong> ${pintar.gender}</p>
-            <p><strong>Ansestro:</strong> ${pintar.ancestry}</p>
-            <p><strong>Varita:</strong> <img src="../src/image/wand.png" alt=""><br>
-            <strong>Madera:</strong> ${pintar.wand.wood}<br>
-            <strong>Núcleo:</strong> ${pintar.wand.core}<br>
-            <strong>Longitud:</strong>${pintar.wand.length}</p>
-            <p><strong>Patronus:</strong> ${pintar.patronus}</p>
-            <p><strong>Actor:</strong> ${pintar.actor}</p>
-            <p><strong>Vivo</strong>: ${pintar.alive}</p>
-          </div>
+const cajaRoles = document.querySelector('#roles');
+const cajaCasas = document.querySelector('#casas');
+const cajaGenero = document.querySelector('#genero');
+
+
+const pintarEnPantalla = (POTTER, cajaImagenes) => {
+  let string = "";
+  POTTER.forEach((obj)=> {
+    string += ` <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="container">
+        <img class="mi-imagen" src="${obj.image}"/>
+        <button id="btn-caracteristicas" class="name">${obj.name}</button>
+      </div>
+      <div class="anverso">
+        <div class="box">
+          <p><strong>Especie:</strong> ${obj.species}</p>
+          <p><strong>Género:</strong> ${obj.gender}</p>
+          <p><strong>Casa:</strong> ${obj.house}</p>
+          <p><strong>Ansestro:</strong> ${obj.ancestry}</p>
+          <p><strong>Varita:</strong> <img src="../src/image/wand.png" alt=""><br>
+          <strong>Madera:</strong> ${obj.wand.wood}<br>
+          <strong>Núcleo:</strong> ${obj.wand.core}<br>
+          <strong>Longitud:</strong>${obj.wand.length}</p>
+          <p><strong>Patronus:</strong> ${obj.patronus}</p>
+          <p><strong>Actor:</strong> ${obj.actor}</p>
+          <p><strong>Vivo</strong>: ${obj.alive}</p>
         </div>
       </div>
     </div>
-    `;
-  miArrImagenes.innerHTML = pintaImagen;
-});
+  </div>`
+  });
+  cajaImagenes.innerHTML = string;
+}
+pintarEnPantalla(POTTER, miArrImagenes);
 
+
+cajaRoles.addEventListener('click', (event) => {
+  // console.log(event.target.id)
+  const rolCapturado =  event.target.id;
+  // console.log(filtroPorRoles(rolCapturado, POTTER));
+  pintarEnPantalla(filtroPorRoles(rolCapturado, POTTER), miArrImagenes);
+})
+
+cajaCasas.addEventListener('click', (event) => {
+  const casaCapturada = event.target.id;
+  pintarEnPantalla(filtroPorCasas(casaCapturada, POTTER), miArrImagenes);
+})
+
+cajaGenero.addEventListener('click', (event) => {
+  const generoCapturada = event.target.id;
+  pintarEnPantalla(filtroPorGenero(generoCapturada, POTTER), miArrImagenes);
+})
 // let casas = {
 //   casa1: 'Gryffindor',
 //   cas2: 'Slythering',
@@ -43,15 +67,14 @@ POTTER.forEach(pintar => {
 //   casa4: 'Revenclaw'
 // }
 
-// let students = filtrar(POTTER, 'hogwartsStudent');
-// let profesores = filtrar(POTTER, 'hogwartsStaff');
 
-// console.log(students);
-// console.log(profesores);
 
-let prueba = probando(POTTER);
-console.log(prueba);
 
+// let prueba = probando(POTTER);
+// console.log(prueba);
+
+// let prueba2 = probando2(POTTER);
+// console.log(prueba2);
 
 
 
