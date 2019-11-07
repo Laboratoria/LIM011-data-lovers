8079d1301307410b10ff3f6507e21b22563f0409
Vilmarys Rodriguez <vilmango@hotmail.com>
2019-11-07 08:32:45 -0500

    Actualiza main y data.js
    Filtro por tipo de pokemon refactorizado usando -filter() y funcional

--- src/data.js
+++ src/data.js
@@ -1,25 +1,48 @@
-/* Manejo de data */
-
-// esta es una función de ejemplo
-
+//  Lista que muestra los pokemones
 export const showPokemon = (data) => {
-  let pokemon ='' 
-  ;
-  data.map(objeto => {
-    pokemon += `
+  let pokemonList = '';
+  data.forEach((objeto) => pokemonList += `
     <div id="${objeto.id}" name="pokemon"><p>
-    <img src="${objeto.img}"/></p>
-    <p>Nombre: ${objeto.name}</p>
-    <p>Peso: ${objeto.weight}</p>
-    <p>Tipo: ${objeto.type.join('  ')}</p>
-    </div>
-    `
-  });
-  //console.log(plantillaPokemon);
-  return pokemon;
+      <p> ${objeto.name} ${objeto.num}</p>
+
+      <img src="${objeto.img}"/></p>
+      <p>Tipo: ${objeto.type.join('  /  ')}</p>
+      <p>Debilidades: ${objeto.weaknesses.join('  /  ')}</p>
+      <p>Peso: ${objeto.weight}</p>
+      <p>Tamaño: ${objeto.height}</p>
+      <p>Caramelos para Evolucionar: ${objeto.candy_count}</p>
+      <p>Huevo: ${objeto.egg}</p>
+    </div>`);
+  return pokemonList;
 };
 
-/*const valorCaramelo = (data) =>{
-  let cuantosCaramelos= `${objeto.candy_count}`;
-  let pokemonBuscado =   console.log(cuanosCaramelos);
-};*/
+// Filtrado de pokemon por tipo
+export const filtroTipoPokemon = (data, tipo) => {
+  // eslint-disable-next-line no-shadow
+  const nuevoArreglo = data.filter((data) => (data.type.indexOf(tipo) !== -1));
+  return nuevoArreglo;
+};
+
+/*  These two work Pokemon Type Filter
+//for loop
+export const filtroPokemon = (data, tipo) =>{
+  const newArray = [];
+  for(let i = 0; i < data.length; i++) {
+    if(data[i].type.indexOf(tipo) !== -1){
+      newArray.push(data[i])
+    }
+  }
+  return newArray;
+};
+
+// FUNCIONA filter
+  export const filtroTipo = (data, tipo) =>{
+const nuevoArreglo =[];
+    data.filter(function(data){
+      if(data.type.indexOf(tipo) !== -1){
+        nuevoArreglo.push(data)
+      }
+    });
+    return nuevoArreglo;
+  };
+  */
--- src/main.js
+++ src/main.js
@@ -1,17 +1,9 @@
-//Manejo DOM
-/*
- * import POKEMON from './data/pokemon/pokemon.js'
- */
-import POKEMON from './data/pokemon/pokemon.js'
+// eslint-disable-next-line import/extensions
+import POKEMON from './data/pokemon/pokemon.js';
 
-import { showPokemon } from './data.js';
-console.log(POKEMON);
-const contenedorPokemon = document.getElementById("muestrapokemon").innerHTML=showPokemon(POKEMON);
+// eslint-disable-next-line import/extensions
+import { showPokemon, filtroTipoPokemon } from './data.js';
 
+const contenedorPokemon = document.getElementById('lista-pokemones').innerHTML = showPokemon(POKEMON);
 
-
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+console.log(filtroTipoPokemon(POKEMON, 'Electric'));
