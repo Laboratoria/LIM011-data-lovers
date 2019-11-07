9afe5339eb8d8b8e1d38794fb3495056ea58c6ed
rubialburqueque <rubi.vap@gmail.com>
2019-11-07 12:40:54 -0500

    unión de cambios

--- src/data.js
+++ src/data.js
@@ -21,25 +21,6 @@ export const showPokemon = (data) => {
   return pokemon;
 };
 
-/* const valorCaramelo = (data) =>{
-  let cuantosCaramelos= `${objeto.candy_count}`;
-  let pokemonBuscado =   console.log(cuanosCaramelos);
-}; */
-  let pokemonList = '';
-  data.forEach((objeto) => pokemonList += `
-    <div id="${objeto.id}" name="pokemon"><p>
-      <p> ${objeto.name} ${objeto.num}</p>
-
-      <img src="${objeto.img}"/></p>
-      <p>Tipo: ${objeto.type.join('  /  ')}</p>
-      <p>Debilidades: ${objeto.weaknesses.join('  /  ')}</p>
-      <p>Peso: ${objeto.weight}</p>
-      <p>Tamaño: ${objeto.height}</p>
-      <p>Caramelos para Evolucionar: ${objeto.candy_count}</p>
-      <p>Huevo: ${objeto.egg}</p>
-    </div>`);
-  return pokemonList;
-};
 
 // Filtrado de pokemon por tipo
 export const filtroTipoPokemon = (data, tipo) => {
@@ -48,26 +29,5 @@ export const filtroTipoPokemon = (data, tipo) => {
   return nuevoArreglo;
 };
 
-/*  These two work Pokemon Type Filter
-//for loop
-export const filtroPokemon = (data, tipo) =>{
-  const newArray = [];
-  for(let i = 0; i < data.length; i++) {
-    if(data[i].type.indexOf(tipo) !== -1){
-      newArray.push(data[i])
-    }
-  }
-  return newArray;
-};
 
-// FUNCIONA filter
-  export const filtroTipo = (data, tipo) =>{
-const nuevoArreglo =[];
-    data.filter(function(data){
-      if(data.type.indexOf(tipo) !== -1){
-        nuevoArreglo.push(data)
-      }
-    });
-    return nuevoArreglo;
-  };
-  */
+
--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import POKEMON from './data/pokemon/pokemon.js';
 // eslint-disable-next-line import/extensions
 import { showPokemon, filtroTipoPokemon } from './data.js';
 
-const contenedorPokemon = document.getElementById('lista-pokemones').innerHTML = showPokemon(POKEMON);
+const contenedorPokemon = document.getElementById('muestrapokemon').innerHTML = showPokemon(POKEMON);
 
 console.log(filtroTipoPokemon(POKEMON, 'Electric'));
