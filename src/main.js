b71a83ab5188f6a809932e716495fca6f1f9da03
Rubí <55994017+rubialburqueque@users.noreply.github.com>
2019-11-11 10:12:42 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,85 +2,39 @@
 import POKEMON from './data/pokemon/pokemon.js';
 
 // eslint-disable-next-line import/extensions
-import { showPokemon, showNamePicturePokemon, showNamePokemon,  filtroTipoPokemon} from './data.js';
+import { showPokemon, showNamePicturePokemon, showNamePokemon, filtroTipoPokemon } from './data.js';
 
 const contenedorPokemon = document.getElementById('muestrapokemon').innerHTML = showPokemon(POKEMON);
 
 console.log(filtroTipoPokemon(POKEMON, 'Electric'));
 
-console.log(showNamePicturePokemon,(POKEMON));
+//console.log(showNamePicturePokemon(POKEMON));
+// Botones funcionalidad de pantallas
 
-console.log(showNamePokemon,(POKEMON));
+const botonDescubrir = document.getElementById('btn-calculate');
+const botonLimpiarCaramelos = document.getElementById('btn-limpiar-caramelos');
 
-const botonDescubrir= document.getElementById('btn-calculate');
-const pantallaPokemonCaramelo= document.getElementById('caramelo-muestra-name-picture-pokemon');
-const botonLimpiarCaramelos=document.getElementById('btn-limpiar-caramelos');
-botonDescubrir.addEventListener('click',()=>{
-document.getElementById('caramelo-muestra-name-picture-pokemon').innerHTML = showNamePicturePokemon(POKEMON);
-pantallaPokemonCaramelo.style.display='block';
-})
-botonLimpiarCaramelos.addEventListener('click',()=>{
-document.getElementById('caramelo-muestra-name-picture-pokemon').innerHTML = '';
-pantallaPokemonCaramelo.style.display='none';
-})
+botonDescubrir.addEventListener('click', () => {
+    document.getElementById('caramelo-muestra-pokemon').innerHTML = showNamePicturePokemon(POKEMON);
+    pantallaCaramelos.style.display = 'block';
+});
 
+botonLimpiarCaramelos.addEventListener('click', () => {
+    document.getElementById('caramelo-muestra-pokemon').innerHTML = '';
+    pantallaCaramelos.style.display = 'none';
+});
 
+const pantallaCaramelos = document.getElementById('pantallacaramelos');
+const botonMostrarCaramelos = document.getElementById('boton-pantalla-caramelos');
+botonMostrarCaramelos.addEventListener('click', () => {
+    pantallaCaramelos.style.display = 'block';
+    pantallaFiltroPokemon.style.display = 'none';
+});
 
-// // CALCULAR DATOS DE EVOLUCION
-// const btnCalculate = document.getElementById('btn-calculate');
-// btnCalculate.addEventListener('click', (e) => {  
-// const validate = (string) => {
-// string = string.toLowerCase();
-// let stringValidate = string.charAt(0).toUpperCase() + string.slice(1);
-// return stringValidate;
-// };
-// const namePokemon = validate(document.getElementById('name-pokemon').value);
-// const candyCount = document.getElementById('candy-count').value;
-// const resultado = pokemon.computeStats(data, namePokemon, candyCount); // data resultado   
-// e.preventDefault();
-// const plantillaResultado = (data) => {
-// let respuesta;
-// for (let i = 0; i < data.length; i++) {
-// if (data[i].pokemonFirstCandy === undefined) {
-// respuesta = `
-// <h1>Tu pokemon ya tuvo todas sus evoluciones :</h1>
-// <p>${data[i].pokemonFirstName}</p>
-// <img class="img-res" src='${data[i].pokemonFirstImg}'>
-// <p> Te sobra ${data[i].pokemonFirstCandyInput}</p>`;
-// } else {
-// if (data[i].pokemonFirstCandyInput < data[i].pokemonFirstCandy) {
-// respuesta = `
-// <h1>Tu pokemon  es :</h1>
-// <p>${data[i].pokemonFirstName}</p>
-// <img class="img-res" src='${data[i].pokemonFirstImg}'>
-// <h1> Tienes  ${data[i].pokemonFirstCandyInput} dulces, te faltan  ${data[i].candyEvolution} para evolucionar a :</h1>                
-// <p>${data[i].pokemonEvolutionName}</p>
-// <img class="img-res" src='${data[i].pokemonEvolutionImg}'>`;
-// } else {
-// respuesta = `
-// <h1>Tu pokemon  es :</h1>
-// <p>${data[i].pokemonFirstName}</p>
-// <img class="img-res" src='${data[i].pokemonFirstImg}'>
-// <h1>Este ya debio evolucionar a:</h1>                                    
-// <p>${data[i].pokemonEvolutionName}</p>
-// <img class="img-res" src='${data[i].pokemonEvolutionImg}'>
-// <h1>Tienes ${data[i].pokemonFirstCandyInput} dulces , te sobran  ${data[i].candyEvolution} para la siguiente evolcion</h1>
-// `;
-// }
-// }
-// }
-// return respuesta;
-// };
-// calculateEvolution.style.display = 'none';
-// evolutionResult.style.display = 'block';
-// resultPokemon.innerHTML = plantillaResultado(resultado);
+const pantallaFiltroPokemon = document.getElementById('selector-filtro-wrap');
+const mostrarPantallaFiltro = document.getElementById('boton-pantalla-filtrotipo');
 
-// btnNew.addEventListener('click', () => {
-// limpiar();
-// calculateEvolution.style.display = 'block';
-// evolutionResult.style.display = 'none';
-// });  
-// });
-// const limpiar = () => {
-// document.getElementById('form').reset();
-// };
+mostrarPantallaFiltro.addEventListener('click', () => {
+    pantallaFiltroPokemon.style.display = 'block';
+    pantallaCaramelos.style.display = 'none';
+});
