95daedfd5b4353cfde3e276517f187fc85791d7b
Vilmarys Rodriguez <vilmango@hotmail.com>
2019-11-11 08:59:26 -0500

    Cambia html, css, main, data
    Algunos cambios en las etiquetas del html y ajustes del css. Con los ajustes correspondientes en el data y main

--- src/main.js
+++ src/main.js
@@ -2,8 +2,56 @@
 import POKEMON from './data/pokemon/pokemon.js';
 
 // eslint-disable-next-line import/extensions
-import { showPokemon, filtroTipoPokemon } from './data.js';
+import { showPokemon, showNamePicturePokemon, showNamePokemon, filtroTipoPokemon } from './data.js';
 
 const contenedorPokemon = document.getElementById('muestrapokemon').innerHTML = showPokemon(POKEMON);
 
 console.log(filtroTipoPokemon(POKEMON, 'Electric'));
+
+//console.log(showNamePicturePokemon(POKEMON));
+// Botones funcionalidad de pantallas
+
+const botonDescubrir = document.getElementById('btn-calculate');
+const botonLimpiarCaramelos = document.getElementById('btn-limpiar-caramelos');
+
+botonDescubrir.addEventListener('click', () => {
+    document.getElementById('caramelo-muestra-pokemon').innerHTML = showNamePicturePokemon(POKEMON);
+    pantallaCaramelos.style.display = 'block';
+});
+
+botonLimpiarCaramelos.addEventListener('click', () => {
+    document.getElementById('caramelo-muestra-pokemon').innerHTML = '';
+    pantallaCaramelos.style.display = 'none';
+});
+
+const pantallaCaramelos = document.getElementById('pantallacaramelos');
+const botonMostrarCaramelos = document.getElementById('boton-pantalla-caramelos');
+botonMostrarCaramelos.addEventListener('click', () => {
+    pantallaCaramelos.style.display = 'block';
+    pantallaFiltroPokemon.style.display = 'none';
+});
+
+const pantallaFiltroPokemon = document.getElementById('selector-filtro-wrap');
+const mostrarPantallaFiltro = document.getElementById('boton-pantalla-filtrotipo');
+
+mostrarPantallaFiltro.addEventListener('click', () => {
+    pantallaFiltroPokemon.style.display = 'block';
+    pantallaCaramelos.style.display = 'none';
+});
+
+
+
+// // eslint-disable-next-line import/extensions
+// import POKEMON from './data/pokemon/pokemon.js';
+
+// // eslint-disable-next-line import/extensions
+// import { showPokemon, filtroTipoPokemon, /*filtroAlfabetico*/} from './data.js';
+
+// const contenedorPokemon = document.getElementById('muestrapokemon').innerHTML = showPokemon(POKEMON);
+
+// console.log(filtroTipoPokemon(POKEMON, 'Electric'));
+// console.log(filtroAlfabetico(POKEMON));
+
+
+
+
