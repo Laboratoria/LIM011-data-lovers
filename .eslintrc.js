ffd71662cc9fcfc6c62291c5982c3e3c228c93d4
NataliTC <natali.tauma@gmail.com>
2019-11-13 11:16:39 -0500

    regla 17

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,17 +22,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-<<<<<<< HEAD
   },
   rules: {
-=======
-    "ecmaFeatures": {
-      "js": true
-  }
-  },
-  rules: {
-  "linebreak-style": 0,
-  "global-require": 0,
->>>>>>> 696e62344b99de5eee1a532e68fa13ea6747a9b0
   },
 };
