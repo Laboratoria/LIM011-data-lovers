cd009fd0c7cc2220c698b6fb6ec800985c2a0fbe
omairapalacios <55103932+omairapalacios@users.noreply.github.com>
2019-11-13 21:36:07 -0500

    add function filterHouse

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import dataPotter from './data/potter/potter.js';
-import { showMainData } from './data.js';
+import { showMainData, filtrarHouse } from './data.js';
 
 showMainData(dataPotter);
+const selectElement = document.querySelector('.select');
+selectElement.addEventListener('change', (event) => {
+  const houseSelected = event.target.value;
+  const dataPotterHouse = filtrarHouse(dataPotter, houseSelected);
+  const dataCharacters = document.querySelector('.data-characters');
+  dataCharacters.querySelectorAll('.card-data').forEach((child) => child.remove());
+  showMainData(dataPotterHouse);
+});
