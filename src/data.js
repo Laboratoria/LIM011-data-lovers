aa967c8d2a49f634447debb232d6ec827db038bf
rubialburqueque <rubi.vap@gmail.com>
2019-11-18 04:54:19 -0500

    homogeniza archivos

--- src/data.js
+++ src/data.js
@@ -1,117 +1,42 @@
-//  Lista que muestra los pokemones
-export const showPokemon = (data) => {
-  let pokemon = '';
-  data.map(item => {
-      pokemon += `
-  <div id="${item.id} "name="pokemon" class="ficha-pokemon">
-  <picture class="fondo-pokemon"><img src="${item.img}"/></picture>
-    <p> ${item.name} ${item.num}</p>
-    <p>Tipo: ${item.type.join('  /  ')}</p>
-    <p>Debilidades: ${item.weaknesses.join('  /  ')}</p>
-<details id="details" class="details">
-  <summary id="summary" class="summary">SABER MÁS</summary>
-  <span>Peso: ${item.weight}<br>
-  Tamaño: ${item.height}<br>
-  Caramelos para Evolucionar: ${item.candy_count}<br>
-  Huevo: ${item.egg}<br></span>
-</details>
-</div>`;
-  });
-  // console.log(plantillaPokemon);
-  return pokemon;
-};
-
 // Filtrado de pokemon por tipo
 export const filtroTipoPokemon = (data, tipo) => {
   // eslint-disable-next-line no-shadow
-  const nuevoArreglo = data.filter((data) => (data.type.indexOf(tipo) !== -1));
-  return nuevoArreglo;
+const nuevoArreglo = data.filter((item) => (item.type.indexOf(tipo) !== -1));
+return nuevoArreglo;
 };
 
-//  Lista que muestra los pokemones(nombre y foto)
-export const showNamePicturePokemon = (data) => {
-  let pokemon = '';
-  data.map(item => {
-      pokemon += `
-  <div class ="singlepoke">
-  <div id="${item.id}"name="pokemon" class="ficha-pokemon">
-  <picture class="fondo-pokemon"><img src="${item.img}"/></picture>
-    <p> ${item.name}</p>
-  </div> 
-  </div>`;
-  });
-  // console.log(plantillaPokemon);
-  return pokemon;
+export const filtroAlfabeticoAZ= (data) =>{
+const nuevoArregloAlfabetico =
+data.sort((a, b) => a.name.localeCompare(b.name)) ;
+return nuevoArregloAlfabetico;
 };
 
-//  Lista que muestra los pokemones(nombre)
-export const showNamePokemon = (data) => {
-  let pokemon = '';
-  data.map(item => {
-      pokemon += `
-  <div id="${item.id}"name="pokemon" class="ficha-pokemon">
-  <p> ${item.name}</p>
-  </div>`;
-  });
-  // console.log(plantillaPokemon);
-  return pokemon;
+export const filtroAlfabeticoZA= (data) =>{
+const nuevoArregloAlfabetico =
+data.sort((a, b) => b.name.localeCompare(a.name)) ;
+return nuevoArregloAlfabetico;
+};
+
+export const ordenNumerico= (data) =>{
+const nuevoArregloNumerico = data.sort((a, b) => a.num.localeCompare(b.num)) ;
+return nuevoArregloNumerico;
+};
+
+export const busquedaPokemonNombre = (data, nombre) =>{
+const pokemonEncontrado = data.filter((data)=> (data.name.toLowerCase() === nombre));
+return pokemonEncontrado;
 };
 
 
-// //  Lista que muestra los pokemones
-// export const showPokemon = (function(data){
-//   let pokemon = ``;
-//   data.map(objeto => {
-//     pokemon += `
-//     <div id="${objeto.id}"name="pokemon" class="ficha-pokemon">
-//     <picture><img src="${objeto.img}"/></picture>
-//       <p> ${objeto.name} ${objeto.num}</p>
-//       <p>Tipo: ${objeto.type.join('  /  ')}</p>
-//       <p>Debilidades: ${objeto.weaknesses.join('  /  ')}</p>
-//   <details id="details" class="details">
-//     <summary id="summary" class="summary">SABER MÁS          </summary>
-//     <span>Peso: ${objeto.weight}<br>
-//     Tamaño: ${objeto.height}<br>
-//     Caramelos para Evolucionar: ${objeto.candy_count}<br>
-//     Huevo: ${objeto.egg}<br></span>
-//   </details>
-//   </div>`;
-//   });
-//   // console.log(plantillaPokemon);
-//   return pokemon;
-// });
-
-
-// // Filtrado de pokemon por tipo
-// export const filtroTipoPokemon = (data, tipo) => {
+//In this function, need to turn the input value to .toLowerCase so it matches
+// export const filtroTipoPokemon2 = (data, tipo) => {
 //   // eslint-disable-next-line no-shadow
-//   const nuevoArreglo = data.filter((data) => (data.type.indexOf(tipo) !== -1));
-//   return nuevoArreglo;
-// };
+//   const nuevoArreglo = data.filter((item) => (item.type.valueOf() == tipo));
+// console.log(nuevoArreglo);
+//   };
 
 
 
 
-// // Función orden alfabético
-// // export const filtroAlfabetico= (data) =>{
-// //   let nuevoArregloAlfabetico =[];
-// //   switch (sorting) {
-// //       case 'AZ': 
-// //       return nuevoArregloAlfabetico = data.sort((a, b) => a.name.localeCompare(b.name)) ;
-      
-// //     break;
-// //     case 'ZA':
-// //         nuevoArregloAlfabetico = data.sort((a, b) => a.name.localeCompare(b.name)) ;
-// //       return nuevoArregloAlfabetico.reverse();
-// //     break;
-// //     default:
-// //       break;
-// // }};
 
-// // export const filtroAlfabeticoAZ= (data) =>{
-// //   const nuevoArregloAlfabetico =
-// //   data.sort((a, b) => a.name.localeCompare(b.name)) ;
-  
-// // return nuevoArregloAlfabetico;
-// // };
 
--- src/main.js
+++ src/main.js
@@ -2,39 +2,110 @@
 import POKEMON from './data/pokemon/pokemon.js';
 
 // eslint-disable-next-line import/extensions
-import { showPokemon, showNamePicturePokemon, showNamePokemon, filtroTipoPokemon } from './data.js';
+import { filtroTipoPokemon, filtroAlfabeticoAZ, filtroAlfabeticoZA, ordenNumerico, busquedaPokemonNombre  } from './data.js';
+//Plantilla Solo imagen y nombre
+const showPicturePokemon = (data) => {
+    let plantillaPokemon = '';
+    data.map(item => {
+        plantillaPokemon += `
+    <div class ="singlepoke">
+        <div id="${item.id}"name="pokemon" class="ficha-pokemon">
+            <picture class="fondo-pokemon"><img src="${item.img}"/></picture>
+            <p>${item.name} #${item.num}</p>
+        </div> 
+    </div>`;
+    });
+return plantillaPokemon;
+};
+//Plantilla con saber más
+const showFichaPokemon = (data) => {
+    let plantillaPokemon = '';
+    data.map(item => {
+        plantillaPokemon += `
+        <div id="${item.id} "name="pokemon" class="ficha-pokemon">
+            <picture class="fondo-pokemon"><img src="${item.img}"/></picture>
+            <p> ${item.name} ${item.num}</p>
+            <p>Tipo: ${item.type.join('  /  ')}</p>
+            <p>Debilidades: ${item.weaknesses.join('  /  ')}</p>
+            <details id="details" class="details">
+            <summary id="summary" class="summary">SABER MÁS</summary>
+            <span>Peso: ${item.weight}<br>
+            Tamaño: ${item.height}<br>
+            Caramelos para Evolucionar: ${item.candy_count}<br>
+            Huevo: ${item.egg}<br></span>
+            </details>
+        </div>`;
+    });
+    // console.log(plantillaPokemon);
+    return plantillaPokemon;
+};
 
-const contenedorPokemon = document.getElementById('muestrapokemon').innerHTML = showPokemon(POKEMON);
+// selecciona el tipo de pokemon de las opciones
+const seleccionTipoPokemon = document.getElementById('pokemon-type');
+//Boton en nav para filtro tipo muestra todo
+const botonMostrarPantallaFiltro= document.getElementById('boton-pantalla-filtrotipo');
+//boton caramelos
+const botonMostrarPantallaCaramelos = document.getElementById('boton-pantalla-caramelos');
+//pantalla de filtro tipo con todo, para ocultar y mostrar
+const pageFiltro = document.getElementById('selector-filtro-wrap');
+//pantalla de caramelos
+const pageEvolucionCaramelos=document.getElementById('pantalla-caramelos');
+//espacio donde muestra la lista de pokemones filtrados
+const listaFiltroTipo=document.getElementById('muestra-lista-pokemon');
+//busqueda por nombre de pokemon
+const inputBusqueda = document.getElementById('busqueda-nombre-pokemon');
 
-console.log(filtroTipoPokemon(POKEMON, 'Electric'));
+const fichaPokemonBuscado=document.getElementById('tarjeta-pokemon-buscado');
 
-//console.log(showNamePicturePokemon(POKEMON));
-// Botones funcionalidad de pantallas
-
-const botonDescubrir = document.getElementById('btn-calculate');
-const botonLimpiarCaramelos = document.getElementById('btn-limpiar-caramelos');
-
-botonDescubrir.addEventListener('click', () => {
-    document.getElementById('caramelo-muestra-pokemon').innerHTML = showNamePicturePokemon(POKEMON);
-    pantallaCaramelos.style.display = 'block';
+inputBusqueda.addEventListener('input', ()=>{
+    fichaPokemonBuscado.innerHTML=(showFichaPokemon(busquedaPokemonNombre(POKEMON, inputBusqueda.value.toLowerCase())))
+    pageFiltro.style.display='block';
 });
 
-botonLimpiarCaramelos.addEventListener('click', () => {
-    document.getElementById('caramelo-muestra-pokemon').innerHTML = '';
-    pantallaCaramelos.style.display = 'none';
+seleccionTipoPokemon.addEventListener('click', ()=>{
+    if (seleccionTipoPokemon.value === 'Todos'){
+        return listaFiltroTipo.innerHTML=showFichaPokemon(POKEMON);
+    } else {
+    return listaFiltroTipo.innerHTML = showFichaPokemon(filtroTipoPokemon(POKEMON, seleccionTipoPokemon.value));
+    }
+});
+botonMostrarPantallaCaramelos.addEventListener('click', ()=>{
+    pageFiltro.style.display='none';
+    pageEvolucionCaramelos.style.display='block';
+    seleccionTipoPokemon.value = '';
+    
+});
+botonMostrarPantallaFiltro.addEventListener('click', ()=>{
+    pageFiltro.style.display='block';
+    pageEvolucionCaramelos.style.display='none';
+    seleccionTipoPokemon.value='';
+    listaFiltroTipo.innerHTML='';
 });
 
-const pantallaCaramelos = document.getElementById('pantallacaramelos');
-const botonMostrarCaramelos = document.getElementById('boton-pantalla-caramelos');
-botonMostrarCaramelos.addEventListener('click', () => {
-    pantallaCaramelos.style.display = 'block';
-    pantallaFiltroPokemon.style.display = 'none';
+const botonOrdenAZ = document.getElementById('az');
+botonOrdenAZ.addEventListener('click', ()=>{
+    if (seleccionTipoPokemon.value === 'Todos'){
+        return listaFiltroTipo.innerHTML=showFichaPokemon(filtroAlfabeticoAZ(POKEMON));
+    } else {
+        return listaFiltroTipo.innerHTML = showFichaPokemon(filtroAlfabeticoAZ(filtroTipoPokemon(POKEMON, seleccionTipoPokemon.value)));
+    }
+});
+const botonOrdenZA = document.getElementById('za');
+botonOrdenZA.addEventListener('click', ()=>{
+    if (seleccionTipoPokemon.value === 'Todos'){
+        return listaFiltroTipo.innerHTML=showFichaPokemon(filtroAlfabeticoZA(POKEMON));
+    } else {
+        return listaFiltroTipo.innerHTML = showFichaPokemon(filtroAlfabeticoZA(filtroTipoPokemon(POKEMON, seleccionTipoPokemon.value)));
+    }
 });
 
-const pantallaFiltroPokemon = document.getElementById('selector-filtro-wrap');
-const mostrarPantallaFiltro = document.getElementById('boton-pantalla-filtrotipo');
+const botonOrdenNumerico = document.getElementById('n');
+botonOrdenNumerico.addEventListener('click', ()=>{
+    if (seleccionTipoPokemon.value === 'Todos'){
+        return listaFiltroTipo.innerHTML=showFichaPokemon(ordenNumerico(POKEMON));
+    } else {
+        return listaFiltroTipo.innerHTML = showFichaPokemon(ordenNumerico(filtroTipoPokemon(POKEMON, seleccionTipoPokemon.value)));
+    }
+}); 
 
-mostrarPantallaFiltro.addEventListener('click', () => {
-    pantallaFiltroPokemon.style.display = 'block';
-    pantallaCaramelos.style.display = 'none';
-});
+
