292347b5094280aec4c96e444e80601854bea8b0
Vilmarys R <43301644+Vilmango@users.noreply.github.com>
2019-11-20 12:55:06 -0500

    Actualiza funciones
    Refactorizado de funciones. Se comentaron las anteriores. Pasaron pruebas de jest

--- src/data.js
+++ src/data.js
@@ -1,31 +1,25 @@
 /* eslint-disable max-len */
-// Filtrado de pokemon por tipo
-export const filtroTipoPokemon = (data, tipo) => {
-  // eslint-disable-next-line no-shadow
-  const nuevoArreglo = data.filter((item) => (item.type.indexOf(tipo) !== -1));
-  return nuevoArreglo;
-};
-
-export const filtroAlfabeticoAZ = (data) => {
-  const nuevoArregloAlfabetico = data.sort((a, b) => a.name.localeCompare(b.name));
-  return nuevoArregloAlfabetico;
-};
-
-export const filtroAlfabeticoZA = (data) => {
-  const nuevoArregloAlfabetico = data.sort((a, b) => b.name.localeCompare(a.name));
-  return nuevoArregloAlfabetico;
-};
-
-export const ordenNumerico = (data) => {
-  const nuevoArregloNumerico = data.sort((a, b) => a.num.localeCompare(b.num));
-  return nuevoArregloNumerico;
-};
-
+export const filtroTipoPokemon = (data, tipo) => data.filter((item) => (item.type.indexOf(tipo) !== -1));
 export const busquedaPokemonNombre = (data, nombre) => data.filter((item) => item.name.toLowerCase().startsWith(nombre));
+export const filtroAlfabeticoAZ = (data) => data.sort((a, b) => a.name.localeCompare(b.name));
+export const filtroAlfabeticoZA = (data) => data.sort((a, b) => b.name.localeCompare(a.name));
+export const ordenNumerico = (data) => data.sort((a, b) => a.num.localeCompare(b.num));
 
-/*  Search function, only shows 1 pokemon if name written correctly and full
+// export const filtroAlfabeticoAZ = (data) => {
+//   const nuevoArregloAlfabetico = data.sort((a, b) => a.name.localeCompare(b.name));
+//   return nuevoArregloAlfabetico;
+// };
+// export const filtroAlfabeticoZA = (data) => {
+//   const nuevoArregloAlfabetico = data.sort((a, b) => b.name.localeCompare(a.name));
+//   return nuevoArregloAlfabetico;
+// };
+// export const ordenNumerico = (data) => {
+//   const nuevoArregloNumerico = data.sort((a, b) => a.num.localeCompare(b.num));
+//   return nuevoArregloNumerico;
+// };
 // export const busquedaPokemonNombre = (data, nombre) => {
-//   // eslint-disable-next-line no-shadow
+// // eslint-disable-next-line no-shadow
 //   const pokemonEncontrado = data.filter((data) => (data.name.toLowerCase() === nombre));
 //   return pokemonEncontrado;
-// }; */
+// };
+
