80eda521fa2748088af450a8b753199c4507bc93
SusanaAtp <gp.susana.11@gmail.com>
2019-11-21 09:08:42 -0500

    realiza css modal y correcciones

--- src/main.js
+++ src/main.js
@@ -100,7 +100,7 @@ const pintadoModal = (extrae) => {
        </div>
        <div class="datos">
        <h3>${personaje[0].house}${coma}${estudiante}${staff}${vivir} </h3>
-       <p><span class= "icon-cake"></span> ${cumpleaños}</p>
+       <h2 class="bday"> <span class= "icon-cake"></span> ${cumpleaños}</h2>
        <p>Especie: ${personaje[0].species}</p>
        <p>Género: ${personaje[0].gender}</p>
        <p>Color de cabello: ${personaje[0].hairColour}</p>
@@ -143,6 +143,7 @@ document.getElementById('menu-personajes').addEventListener('click', () => {
   casas.classList.add('ocultar');
   rol.classList.add('ocultar');
   personajes.classList.remove('ocultar');
+  document.querySelector('#modalP').classList.add('ocultar');
   document.querySelector('.filtrar-personajes').reset();
   divTodosP.innerHTML = pintado(ordenado(POTTER), 'image', 'name', '');
   datos = POTTER;
