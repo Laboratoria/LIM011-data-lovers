ecd7b29013199d07f8a26ded1cccf18e344afad4
Vilmarys Rodriguez <vilmango@hotmail.com>
2019-11-27 10:06:27 -0500

    Actualiza main.js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,25 @@ import {
   filtroTipoPokemon, filtroAlfabeticoAZ, filtroAlfabeticoZA, ordenNumerico, busquedaPokemonNombre, evolucionCaramelos,
 } from './data.js';
 
+const seleccionTipoPokemon = document.getElementById('pokemon-type');
+const botonMostrarPantallaFiltro = document.getElementById('boton-pantalla-filtrotipo');
+const botonMostrarPantallaCaramelos = document.getElementById('boton-pantalla-caramelos');
+// pantalla de filtro tipo con todo, para ocultar y mostrar
+const pageFiltro = document.getElementById('selector-filtro-wrap');
+// pantalla de caramelos
+const pageEvolucionCaramelos = document.getElementById('pantalla-caramelos');
+// espacio donde muestra la lista de pokemones filtrados
+const listaFiltroTipo = document.getElementById('muestra-lista-pokemon');
+// busqueda por nombre de pokemon
+const inputBusqueda = document.getElementById('busqueda-nombre-pokemon');
+const fichaPokemonBuscado = document.getElementById('tarjeta-pokemon-buscado');
+// Evolucion por caramelos
+const nameCandyPokemon = document.getElementById('busqueda-nombre-pokemon-caramelos');
+const candyCountUsuario = document.getElementById('candy-count');
+const botonDescubrir = document.getElementById('btn-calculate');
+const botonLimpiarCaramelos = document.getElementById('btn-limpiar-caramelos');
+const muestraEvolucion = document.getElementById('caramelo-muestra-pokemon');
+
 // Plantilla Solo imagen y nombre
 const showPicturePokemon = (data) => {
   let plantillaPokemon = '';
@@ -21,8 +40,7 @@ const showPicturePokemon = (data) => {
   });
   return plantillaPokemon;
 };
-
-// Plantilla buscador por nombre, añade al datalist los nombres directo del pokemon.js
+// Muestra lista de nombres de pokemon en el datalist
 const opcion = (data) => {
   const datalistName = document.getElementById('pokemon-name');
   // debugger;
@@ -33,7 +51,30 @@ const opcion = (data) => {
   }
 };
 opcion(POKEMON);
-
+// Plantilla para mostrar evolucion por caramelos
+const showEvolucionCaramelos = (data) => {
+  for (let i = 0, len = showEvolucionCaramelos.length; i < len; i += 1) {
+    if (data[i].numero === undefined) {
+      muestraEvolucion.innerHTML = `
+    <div id="${data[i].id}" name="pokemon" class="ficha-pokemon">
+          <div class="fondo-pokemon">
+            <p>${data[i].mensaje}</p>
+            <img src="${data[i].img}"/>
+          </div>
+    </div>`;
+    } else {
+    // Evoluciona
+      muestraEvolucion.innerHTML = `
+    <div id="${data[i].id}" name="pokemon" class="ficha-pokemon">
+          <div class="fondo-pokemon">
+              <p>${data[i].mensaje}</p>
+              <img src="${data[i].img}"/> <p>${data[i].mensaje2}</p>
+              <img src="${data[i].imgEvo}"/>
+          </div>
+    </div>`;
+    }
+  }
+};
 // Plantilla con saber más
 const showFichaPokemon = (data) => {
   let plantillaPokemon = '';
@@ -45,51 +86,55 @@ const showFichaPokemon = (data) => {
             <p>Tipo: ${item.type.join('  /  ')}</p>
             <p>Debilidades: ${item.weaknesses.join('  /  ')}</p>
             <details id="details" class="details">
-            <summary id="summary" class="summary">SABER MÁS</summary>
-            <span>Peso: ${item.weight}<br>
-            Tamaño: ${item.height}<br>
-            Caramelos para Evolucionar: ${item.candy_count}<br>
-            Huevo: ${item.egg}<br></span>
+              <summary id="summary" class="summary">SABER MÁS</summary>
+              <span>Peso: ${item.weight}<br>
+              Tamaño: ${item.height}<br>
+              Caramelos para Evolucionar: ${item.candy_count}<br>
+              Huevo: ${item.egg}<br></span>
             </details>
         </div>`;
   });
   // console.log(plantillaPokemon);
   return plantillaPokemon;
 };
-
-// selecciona el tipo de pokemon de las opciones
-const seleccionTipoPokemon = document.getElementById('pokemon-type');
-// Boton en nav para filtro tipo muestra todo
-const botonMostrarPantallaFiltro = document.getElementById('boton-pantalla-filtrotipo');
-// boton caramelos
-const botonMostrarPantallaCaramelos = document.getElementById('boton-pantalla-caramelos');
-// pantalla de filtro tipo con todo, para ocultar y mostrar
-const pageFiltro = document.getElementById('selector-filtro-wrap');
-// pantalla de caramelos
-const pageEvolucionCaramelos = document.getElementById('pantalla-caramelos');
-// espacio donde muestra la lista de pokemones filtrados
-const listaFiltroTipo = document.getElementById('muestra-lista-pokemon');
-// busqueda por nombre de pokemon
-const inputBusqueda = document.getElementById('busqueda-nombre-pokemon');
-
-const fichaPokemonBuscado = document.getElementById('tarjeta-pokemon-buscado');
-
+// Plantilla con saber más
+const showFichaPokemonBusqueda = (data) => {
+  let plantillaPokemon = '';
+  data.forEach((item) => {
+    plantillaPokemon += `
+    <div id="${item.id}" name="pokemon" class="ficha-pokemon">
+            <div class="fondo-pokemon"><img src="${item.img}"/></div>
+            <p> ${item.name} ${item.num}</p>
+            <details id="details" class="details">
+              <summary id="summary" class="summary">SABER MÁS</summary>
+              <span>Tipo: ${item.type.join('  /  ')}
+            <br>Debilidades: ${item.weaknesses.join('  /  ')}
+            Peso: ${item.weight}<br>
+            Tamaño: ${item.height}<br>
+            Caramelos para Evolucionar: ${item.candy_count}<br>
+            Huevo: ${item.egg}</span>
+            </details>
+        </div>`;
+  });
+  // console.log(plantillaPokemon);
+  return plantillaPokemon;
+};
 inputBusqueda.addEventListener('input', () => {
   if (inputBusqueda.value.toLowerCase() === '') {
     fichaPokemonBuscado.innerHTML = '';
-    // return fichaPokemonBuscado;
+    pageFiltro.style.display = 'none';
+  } else {
+    fichaPokemonBuscado.innerHTML = (showFichaPokemonBusqueda(busquedaPokemonNombre(POKEMON, inputBusqueda.value.toLowerCase())));
+    pageFiltro.style.display = 'block';
   }
-  fichaPokemonBuscado.innerHTML = (showFichaPokemon(busquedaPokemonNombre(POKEMON, inputBusqueda.value.toLowerCase())));
-  pageFiltro.style.display = 'block';
   // return fichaPokemonBuscado;
 });
-
 seleccionTipoPokemon.addEventListener('click', () => {
   if (seleccionTipoPokemon.value === 'Todos') {
-    listaFiltroTipo.innerHTML = showPicturePokemon(POKEMON);
+    listaFiltroTipo.innerHTML = showFichaPokemon(POKEMON);
     return listaFiltroTipo;
   }
-  listaFiltroTipo.innerHTML = showPicturePokemon(filtroTipoPokemon(POKEMON, seleccionTipoPokemon.value));
+  listaFiltroTipo.innerHTML = showFichaPokemon(filtroTipoPokemon(POKEMON, seleccionTipoPokemon.value));
   return listaFiltroTipo;
 });
 botonMostrarPantallaCaramelos.addEventListener('click', () => {
@@ -102,14 +147,16 @@ botonMostrarPantallaFiltro.addEventListener('click', () => {
   pageEvolucionCaramelos.style.display = 'none';
   seleccionTipoPokemon.value = '';
   listaFiltroTipo.innerHTML = '';
+  document.getElementById('caramelo-muestra-pokemon').innerHTML = '';
+  document.getElementById('form').reset();
 });
 
 const botonOrdenAZ = document.getElementById('az');
 botonOrdenAZ.addEventListener('click', () => {
   if (seleccionTipoPokemon.value === 'Todos') {
-    listaFiltroTipo.innerHTML = showPicturePokemon(filtroAlfabeticoAZ(POKEMON));
+    listaFiltroTipo.innerHTML = showFichaPokemon(filtroAlfabeticoAZ(POKEMON));
   }
-  listaFiltroTipo.innerHTML = showPicturePokemon(filtroAlfabeticoAZ(filtroTipoPokemon(POKEMON, seleccionTipoPokemon.value)));
+  listaFiltroTipo.innerHTML = showFichaPokemon(filtroAlfabeticoAZ(filtroTipoPokemon(POKEMON, seleccionTipoPokemon.value)));
 });
 const botonOrdenZA = document.getElementById('za');
 botonOrdenZA.addEventListener('click', () => {
@@ -118,7 +165,6 @@ botonOrdenZA.addEventListener('click', () => {
   }
   listaFiltroTipo.innerHTML = showPicturePokemon(filtroAlfabeticoZA(filtroTipoPokemon(POKEMON, seleccionTipoPokemon.value)));
 });
-
 const botonOrdenNumerico = document.getElementById('n');
 botonOrdenNumerico.addEventListener('click', () => {
   if (seleccionTipoPokemon.value === 'Todos') {
@@ -127,46 +173,11 @@ botonOrdenNumerico.addEventListener('click', () => {
   listaFiltroTipo.innerHTML = showPicturePokemon(ordenNumerico(filtroTipoPokemon(POKEMON, seleccionTipoPokemon.value)));
 });
 
-// Plantilla para evolucion Pokemon
-const nameCandyPokemon = document.getElementById('busqueda-nombre-pokemon-caramelos');
-const candyCountUsuario = document.getElementById('candy-count');
-const botonDescubrir = document.getElementById('btn-calculate');
-
-
-const showEvolucionCaramelos = (data) => {
-  for (let i = 0, len = showEvolucionCaramelos.length; i < len; i += 1) {
-    if (data[i].numero === undefined) {
-    // Ya no evoluciona
-      document.getElementById('caramelo-muestra-pokemon').innerHTML = `
-    <div id="${data[i].id}" name="pokemon" class="ficha-pokemon">
-           <div class="fondo-pokemon">
-               <p>${data[i].mensaje}</p>
-               <img src="${data[i].img}"/></div>
-          </div>`;
-    } else {
-    // Evoluciona
-      document.getElementById('caramelo-muestra-pokemon').innerHTML = `
-    <div id="${data[i].id}" name="pokemon" class="ficha-pokemon">
-           <div class="fondo-pokemon">
-               <p>${data[i].mensaje}</p>
-               <img src="${data[i].img}"/> <p>${data[i].mensaje2}</p>
-               <img src="${data[i].imgEvo}"/>
-               </div>
-          </div>`;
-    }
-  }
-};
-showEvolucionCaramelos(evolucionCaramelos(POKEMON, nameCandyPokemon.value, candyCountUsuario.value));
-
-// console.log(evolucionCaramelos(POKEMON, nameCandyPokemon.value, candyCountUsuario.value));
-
 botonDescubrir.addEventListener('click', () => {
   // eslint-disable-next-line no-restricted-globals
   event.preventDefault();
-  document.getElementById('caramelo-muestra-pokemon').innerHTML = evolucionCaramelos(POKEMON, nameCandyPokemon.value, candyCountUsuario.value);
+  showEvolucionCaramelos(evolucionCaramelos(POKEMON, nameCandyPokemon.value, candyCountUsuario.value));
 });
-
-const botonLimpiarCaramelos = document.getElementById('btn-limpiar-caramelos');
 
 botonLimpiarCaramelos.addEventListener('click', () => {
   document.getElementById('caramelo-muestra-pokemon').innerHTML = '';
