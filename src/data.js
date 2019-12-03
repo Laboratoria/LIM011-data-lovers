63dced17e8cc5c17f4b3a711eb1f867f4377d09f
BarsadeJesus <pattycbs28@gmail.com>
2019-12-03 18:33:31 -0500

    3-12-2019

--- src/data.js
+++ src/data.js
@@ -1,23 +1,21 @@
 export const traerDataPokemon = (arr) => {
   const newArray = [];
   for (let i = 0; i < arr.length; i += 1) {
-      newArray.push({ 
-        identificador: arr[i].id,
-        nombre: arr[i].name,
-        imagen: arr[i].img,
-        altura: arr[i].height,
-        peso: arr[i].weight,
-        tipo: arr[i].type,
-        cant_caramelos: arr[i].candy_count,
-        caramelos: arr[i].candy,
-        multiplicador: arr[i].multipliers,
-        debilidades: arr[i].weaknesses,
-        pre_evolucion: arr[i].prev_evolution,
-        siguiente_evolucion: arr[i].next_evolution,
-
-      });   
-   }
-   //console.log(newArray[61].pre_evolucion[0].num);
+    newArray.push({
+      identificador: arr[i].id,
+      nombre: arr[i].name,
+      imagen: arr[i].img,
+      altura: arr[i].height,
+      peso: arr[i].weight,
+      tipo: arr[i].type,
+      cant_caramelos: arr[i].candy_count,
+      caramelos: arr[i].candy,
+      multiplicador: arr[i].multipliers,
+      debilidades: arr[i].weaknesses,
+      pre_evolucion: arr[i].prev_evolution,
+      siguiente_evolucion: arr[i].next_evolution,
+    });
+  }
   return newArray;
 };
 
@@ -30,45 +28,30 @@ export const ordenarAscOdescData = (arr, string) => {
   return arr;
 };
 
-export const filtrarPokemones = (arr, tPokemones) => {
-  const arregloFiltradoPokemones = [];
-  for (let i = 0; i < arr.length; i += 1) {
-    const tipoFiltrado = arr[i].type;
-    const resultado = tipoFiltrado.filter(elemento => elemento === tPokemones);
-    if (resultado != '') {
-      arregloFiltradoPokemones.push(arr[i]);
-    }
-  }
-  return arregloFiltradoPokemones;
-};
-
 // export const filtrarPokemones = (arr, tPokemones) => {
-// const arregloFiltradoPokemones = [];
-// for (let i = 0; i < arr.length; i += 1) {
-// const tipoFiltrado = arr[i].type;
-// for (let j = 0; j < tipoFiltrado.length; j += 1){
-// if (tipoFiltrado[j] === tPokemones) {
-// arregloFiltradoPokemones.push(arr[i]);
-// }
-// }
-// }
-// return arregloFiltradoPokemones;
+//   const arregloFiltradoPokemones = [];
+//   for (let i = 0; i < arr.length; i += 1) {
+//     const tipoFiltrado = arr[i].type;
+//     const resultado = tipoFiltrado.filter((elemento) => elemento === tPokemones);
+//     if (resultado !== '') {
+//       arregloFiltradoPokemones.push(arr[i]);
+//     }
+//   }
+//   return arregloFiltradoPokemones;
 // };
 
-export const filtrarPokemonesId = (arr, IdNextEvolution) => {
-  const arrFiltradoPokemonesId = [];
-  for (let i = 0; i < arr.length; i += 1) {
-    const next_evolutionFiltrado = arr[i].next_evolution;
-    for (let j = 0; j < next_evolutionFiltrado.length; j += 1){
-    if (next_evolutionFiltrado[j] === IdNextEvolution) {
-      arrFiltradoPokemonesId.push(arr[j]);
-    }
-  }
+export const filtrarPokemones = (arr, tPokemones) => {
+const arregloFiltradoPokemones = [];
+for (let i = 0; i < arr.length; i += 1) {
+const tipoFiltrado = arr[i].type;
+for (let j = 0; j < tipoFiltrado.length; j += 1){
+if (tipoFiltrado[j] === tPokemones) {
+arregloFiltradoPokemones.push(arr[i]);
 }
-console.log(arrFiltradoPokemonesId.push(arr[0]))
-  return arrFiltradoPokemonesId;
- };
-
+}
+}
+return arregloFiltradoPokemones;
+};
 
 export const mostrarTop = (arr) => {
   arr.sort((p1, p2) => ((p1.spawn_chance > p2.spawn_chance) ? -1 : 1));
@@ -82,18 +65,16 @@ export const buscarPokemon = (arr, nombrePokemonBuscar) => {
   return arrBuscaPokemon;
 };
 
-export const buscador = (arr, nombrePokemonBuscar) => {
-return arr.filter((obj) => obj.name.toLowerCase().startsWith(nombrePokemonBuscar));
+export const buscadorPrimerasLetrasNombre = (arr, nombrePokemonBuscar) => {
+  return arr.filter((obj) => obj.name.toLowerCase().startsWith(nombrePokemonBuscar));
 };
 
 export const filtrarPorCandy = (arr, varCaramelos) => {
   const arrfiltraPokemonPorCandy = [];
-  for(let i=0; i<arr.length; i++){
-    if(arr[i].candy===varCaramelos){
-      arrfiltraPokemonPorCandy.push(arr[i].img)
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i].candy === varCaramelos) {
+      arrfiltraPokemonPorCandy.push(arr[i].img);
     }
   }
   return arrfiltraPokemonPorCandy;
-  
 };
-
--- src/main.js
+++ src/main.js
@@ -1,79 +1,75 @@
 
 import POKEMON from './data/pokemon/pokemon.js';
 
-
 import {
   traerDataPokemon,
   ordenarAscOdescData,
   filtrarPokemones,
   mostrarTop,
   buscarPokemon,
-  buscador,
+  buscadorPrimerasLetrasNombre,
   filtrarPorCandy,
 } from './data.js';
-import pokemon from './data/pokemon/pokemon.js';
 
 const radioInput = document.querySelectorAll('input[name=ordena]');
 
-
-const containerElements= (obj) => {
+const containerElements = (obj) => {
   const divElement = document.createElement("div");
-  const idPok=obj.identificador;
-  //filtrarPokemonesId
-  
   divElement.innerHTML = `
   <img class="imagenPokemon" src = "${obj.imagen}"/>
   <h1>${obj.identificador}</h1><p>${obj.nombre}</p>
   `;
-  
   divElement.addEventListener('click', () => {
-    
-    //console.log(filtrarPorCandy((POKEMON),varCandy))
     const divElem = document.createElement('div');
-    
     divElem.classList.add('modalDialog');
-    const varCandy=obj.caramelos;
-    console.log(varCandy);
     divElem.innerHTML = `
     <div>
       <a href = "#close" title = "Close" class = "close">X</a>
       <seccion>
       <h2>${obj.nombre.toUpperCase()}</h2>
-      <img class="imagenPokemon" src = "${obj.imagen}"/>
+      <img class ="imagenPokemon" src = "${obj.imagen}"/>
       </seccion>
       <seccion>
       <p>Peso: ${obj.peso}  Altura: ${obj.altura} Tipo: ${obj.tipo}</p>
       <p>Caramelos: ${obj.cant_caramelos}</p> 
       <p>Multiplicador: ${obj.multiplicador}</p>
       </seccion>
-      <seccion id="evoluciones">
-      </seccion>
+      <seccion class="evolucion">
+      <figure id="evoluciones">
+      </figure></seccion>
     </div> 
     `;
     
     document.getElementById("contenedor-modal").appendChild(divElem);
 
     const arregloCaramelos = filtrarPorCandy((POKEMON), obj.caramelos);
-    
-    if(obj.caramelos !== undefined){
-      for (let i=0; i< arregloCaramelos.length; i++){
-        
+    if(obj.caramelos !== 'None'){
+      for (let i = 0; i < arregloCaramelos.length; i += 1) {
         document.getElementById("evoluciones").innerHTML += ` 
-        <img class="imagenPokemon" src="${arregloCaramelos[i]}"/>
+        <img class ="imagenPokemon" src ="${arregloCaramelos[i]}"/>
         `;
         document.querySelector("#contenedor-modal").appendChild(divElem);
       }
     };
 
+    // const arregloCaramelos = filtrarPorCandy((POKEMON), obj.caramelos);
+    // if(obj.caramelos !== 'None'){
+    //   for (let i = 0; i < arregloCaramelos.length; i += 1) {
+    //     const seccionPokemonEv=document.createElement('seccion');
+    //     seccionPokemonEv.setAttribute('id', 'evoluciones');
+    //     document.getElementById('evoluciones').innerHTML += ` 
+    //     <img class ="imagenPokemon" src ="${arregloCaramelos[i]}"/>
+    //     `;
+    //     document.querySelector("#contenedor-modal").appendChild(divElem);
+    //   }
+    // };
+
     divElem.style.display = 'block';
     divElem.querySelector('.close').addEventListener('click', () => {
     divElem.classList.remove("modalDialog");
-    document.querySelector('#contenedor-modal').innerHTML='';
+    document.querySelector('#contenedor-modal').innerHTML = '';
     });
-    
-  //divElement.appendChild(divElem);
   });
-
   return divElement;
 }
 
@@ -82,31 +78,23 @@ const generarTemplatePokemones = (arr) => {
     document.querySelector('#contenedor-pokemons').appendChild(containerElements(obj));
   });
 };
-
-
 generarTemplatePokemones(traerDataPokemon(POKEMON));
-
-
 const inputBuscaPokemon = document.getElementById('buscaPokemon');
 inputBuscaPokemon.addEventListener('click', () => {
   const nombrePokemonBuscar = document.getElementById('buscar').value;
   const muestraPokemon = traerDataPokemon(buscarPokemon((POKEMON), nombrePokemonBuscar));
   document.querySelector('#contenedor-pokemons').innerHTML='';
   generarTemplatePokemones(muestraPokemon);
-
 });
-
-
 const desple = document.getElementById('desple');
 desple.addEventListener('click', (event) => {
   const tPokemones = event.target.id;
   const arregloFiltrado = traerDataPokemon(filtrarPokemones((POKEMON), tPokemones));
-  document.querySelector('#contenedor-pokemons').innerHTML='';
+  document.querySelector('#contenedor-pokemons').innerHTML = '';
   generarTemplatePokemones(arregloFiltrado);
 });
-
 for (let i = 0; i < radioInput.length; i += 1) {
-radioInput[i].addEventListener('change', (event) => {
+  radioInput[i].addEventListener('change', (event) => {
     const string = event.target.id;
     const arregloOrdenado = traerDataPokemon(ordenarAscOdescData((POKEMON), string));
     document.querySelector('#contenedor-pokemons').innerHTML='';
@@ -116,14 +104,12 @@ radioInput[i].addEventListener('change', (event) => {
 const btnBuscarTop10 = document.getElementById('botonBuscar');
 btnBuscarTop10.addEventListener('click', () => {
   const arregloMuestraTop = traerDataPokemon(mostrarTop(POKEMON));
-  document.querySelector('#contenedor-pokemons').innerHTML='';
+  document.querySelector('#contenedor-pokemons').innerHTML = '';
   generarTemplatePokemones(arregloMuestraTop);
 });
-
 const buscaNombre=document.getElementById('buscar');
 buscaNombre.addEventListener('input',(event) => {
-  const pokeBuscado = traerDataPokemon(buscador((POKEMON), event.target.value));
-  document.querySelector('#contenedor-pokemons').innerHTML='';
+  const pokeBuscado = traerDataPokemon(buscadorPrimerasLetrasNombre((POKEMON), event.target.value));
+  document.querySelector('#contenedor-pokemons').innerHTML = '';
   generarTemplatePokemones(pokeBuscado);
 })
-
