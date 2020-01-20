a9c8fd0aa68917c4f23c7519d860001a90df7192
LIZBETHJAICO <lizbethjaico@gmail.com>
2020-01-19 22:55:22 -0500

    archivo recuperado

--- src/data.js
+++ src/data.js
@@ -1,7 +1,105 @@
-/* Manejo de data */
 
-// esta es una función de ejemplo
+import {
+  objecVarita, casa,
+} from './data/potter/extras.js';
 
-export const example = () => {
-  return 'example';
+export const agregarImagenCasa = (obj) => {
+  let imagenCasa;
+  // eslint-disable-next-line default-case
+  switch (obj.house) {
+    case 'Gryffindor':
+      imagenCasa = casa.Gryffindor;
+      break;
+    case 'Hufflepuff':
+      imagenCasa = casa.Hufflepuff;
+      break;
+    case 'Ravenclaw':
+      imagenCasa = casa.Ravenclaw;
+      break;
+    case 'Slytherin':
+      imagenCasa = casa.Slytherin;
+      break;
+    case '':
+      imagenCasa = casa.noexiste;
+      break;
+  }
+  return imagenCasa;
+};
+export const agregarRol = (obj) => {
+  let rol = '';
+  // eslint-disable-next-line no-lone-blocks
+  if (obj.hogwartsStudent) {
+    rol = 'Student';
+  } if (obj.hogwartsStaff) {
+    rol = 'Staff';
+  }
+  if (!obj.hogwartsStaff && !obj.hogwartsStudent) {
+    rol = 'no tiene rol';
+  }
+  return rol;
+};
+
+export const agregarVarita = (obj) => {
+  let imageNucleo = '';
+  // eslint-disable-next-line default-case
+  switch (obj.wand.core) {
+    case 'dragon heartstring':
+      imageNucleo = objecVarita.dragon;
+      break;
+    case 'unicorn tail-hair':
+      imageNucleo = objecVarita.unicorn;
+      break;
+    case 'phoenix feather':
+      imageNucleo = objecVarita.phonix;
+      break;
+    case '':
+      imageNucleo = '';
+      break;
+  }
+  return imageNucleo;
+};
+
+
+// eslint-disable-next-line arrow-body-style
+export const agregarNewData = (data) => {
+  return data.map((obj) => ({
+    imagenCasa: agregarImagenCasa(obj),
+    name: obj.name,
+    image: obj.image,
+    species: obj.species,
+    gender: obj.gender,
+    house: obj.house,
+    dateOfBirth: obj.dateOfBirth,
+    actor: obj.actor,
+    wood: obj.wand.wood,
+    core: obj.wand.core,
+    length: obj.wand.length,
+    nucleo: agregarVarita(obj),
+    rol: agregarRol(obj),
+    patronus: obj.patronus,
+  }));
+};
+
+
+export const filtrar = (data, prop, condicion) => {
+  const filterArray = data.filter((obj) => (obj[prop] === condicion));
+  return filterArray;
+};
+
+export const filterPatronus = (dataPotter) => {
+  const dataPatronus = dataPotter.filter((data) => (data.patronus !== ''));
+  return dataPatronus;
+};
+export const filterVarita = (dataPotter) => {
+  // eslint-disable-next-line no-mixed-operators
+  const dataVarita = dataPotter.filter((data) => ((data.wood !== '') || (data.core !== '') || (data.length !== '')));
+  return dataVarita;
+};
+
+
+// eslint-disable-next-line max-len
+
+export const searchName = (data, name) => {
+  const search = data.filter((obj) => (obj.name.toLowerCase().indexOf(name.toLowerCase()) > -1));
+  return search;
 };
---
+++ src/data/potter/extras.js
@@ -0,0 +1,14 @@
+
+export const objecVarita = {
+  dragon: 'img/dragon.jpg',
+  unicorn: 'img/unicorn.jpg',
+  phonix: 'img/phoenix.jpg',
+};
+
+export const casa = {
+  Gryffindor: 'img/casa/gryffindor.jpg',
+  Hufflepuff: 'img/casa/Hufflepuff.jpg',
+  Ravenclaw: 'img/casa/ravenclaw.jpg',
+  Slytherin: 'img/casa/slytherin.png',
+  noexiste: 'img/casa/pregunta.jpg',
+};
